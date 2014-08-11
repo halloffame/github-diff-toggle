ab5b83fa68e1028193c3976450b8e61e31d355ca
Ryan Hall <ryan.hall@biola.edu>
2014-08-11 16:15:19 -0700

    Github changed their html structure. This should fix what broke with the new changes.

--- content_script.js
+++ content_script.js
@@ -1,30 +1,32 @@
 function hide(selector) {
   var list = document.querySelectorAll(selector);
   for (var i = 0; i < list.length; ++i) {
-    list[i].style.display = 'none';
+    // You have to reference the parentNode because what you select is a cell inside the row.
+    //  There is no way to tell from the row if it is an addition or deletion.
+    list[i].parentNode.style.display = 'none';
   }
 }
 
 function show(selector) {
   var list = document.querySelectorAll(selector);
   for (var i = 0; i < list.length; ++i) {
-    list[i].style.display = '';
+    list[i].parentNode.style.display = '';
   }
 }
 
 switch(document.body.dataset.visible_diff) {
   case 'i':
-    hide('.file-diff-line.gi'); // Insertion
-    show('.file-diff-line.gd'); // Deletion
+    hide('.diff-line-code-addition');
+    show('.diff-line-code-deletion');
     document.body.dataset.visible_diff = 'd';
     break;
   case 'd':
-    show('.file-diff-line.gi'); // Insertion
-    show('.file-diff-line.gd'); // Deletion
+    show('.diff-line-code-addition');
+    show('.diff-line-code-deletion');
     document.body.dataset.visible_diff = 'off';
     break;
   default:
-    show('.file-diff-line.gi'); // Insertion
-    hide('.file-diff-line.gd'); // Deletion
+    show('.diff-line-code-addition');
+    hide('.diff-line-code-deletion');
     document.body.dataset.visible_diff = 'i';
 }
